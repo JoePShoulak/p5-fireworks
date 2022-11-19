b82bb799b6589aef4a863f9678e5769fd82f36be
Joe P Shoulak <joe@squarecompass.io>
2022-11-18 22:02:42 -0600

    change explode to burst

--- libraries/Firework.js
+++ libraries/Firework.js
@@ -1,27 +1,27 @@
 class Firework {
   static vStartMax = -15;
   static gravity = new Vector(0, 0.2);
-  static explodeCount = 100;
-  static explodeVel = 3;
+  static burstCount = 100;
+  static burstVel = 3;
   static updateCallback() {}
 
   constructor() {
     const seed = new Particle(Math.random() * width, height, "white");
     seed.vel = new Vector(0, Math.random() * Firework.vStartMax);
     this.particles = [seed];
-    this.exploded = false;
+    this.bursted = false;
   }
 
-  explode() {
+  burst() {
     const { pos } = this.particles.pop();
 
-    for (let i = 0; i < Firework.explodeCount; i++) {
+    for (let i = 0; i < Firework.burstCount; i++) {
       const newP = new Particle(pos.x, pos.y);
-      newP.vel = Vector.random.mult(Firework.explodeVel);
+      newP.vel = Vector.random.mult(Firework.burstVel);
       this.particles.push(newP);
     }
 
-    this.exploded = true;
+    this.bursted = true;
   }
 
   update() {
@@ -30,7 +30,7 @@ class Firework {
       p.update();
     });
 
-    if (this.particles[0]?.vel.y > 0 && !this.exploded) this.explode();
+    if (this.particles[0]?.vel.y > 0 && !this.bursted) this.burst();
 
     Firework.updateCallback(this);
   }
